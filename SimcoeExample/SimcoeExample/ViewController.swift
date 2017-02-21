daf14ee9aecc63eb35016c5da1063ab7b7a1f331
Jonathan Samudio <jonathan@prolificinteractive.com>
2017-02-21 16:24:52 -0500

    Added simcoe example project.

---
+++ SimcoeExample/SimcoeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimcoeExample
+//
+//  Created by Jonathan Samudio on 2/21/17.
+//  Copyright © 2017 Prolific Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
