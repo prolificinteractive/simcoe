29b5c399d5d0e75185c626dd74e8da120adc3c85
Jonathan Samudio <jonathan@prolificinteractive.com>
2017-02-22 10:52:52 -0500

    Updated the simcoe example project with new class file structure.

---
+++ SimcoeExample/SimcoeExample/TrackerActions/PageViewTrackingAction.swift
@@ -0,0 +1,9 @@
+//
+//  PageViewTrackingAction.swift
+//  SimcoeExample
+//
+//  Created by Jonathan Samudio on 2/22/17.
+//  Copyright © 2017 Prolific Interactive. All rights reserved.
+//
+
+import Foundation
