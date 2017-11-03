447e7989331f1c70d4fd67716a808d874182072a
Yoseob Lee <yoseob@prolificinteractive.com>
2017-11-03 16:26:30 -0400

    Expand SimcoeProduct and update documentation

--- Simcoe/SimcoeProduct.swift
+++ Simcoe/SimcoeProduct.swift
@@ -21,6 +21,21 @@ public struct SimcoeProduct {
     /// The price.
     public let price: Double?
 
+    /// The product brand.
+    public let brand: String?
+
+    /// The product category.
+    public let category: String?
+
+    /// The coupon associated with the product.
+    public let couponCode: String?
+
+    /// The product variant.
+    public let variant: String?
+
+    /// The position of the product.
+    public let position: UInt?
+
     /// The properties.
     public let properties: Properties?
 
@@ -30,17 +45,32 @@ public struct SimcoeProduct {
     ///   - productName: The product name.
     ///   - productId: The product Id.
     ///   - quantity: The quantity.
-    ///   - price: The price.
-    ///   - properties: The properties.
+    ///   - price: Optional. The price.
+    ///   - brand: Optional. The product brand. Defaults to nil.
+    ///   - category: Optional. The product category. Defaults to nil.
+    ///   - couponCode: Optional. The coupon associated with the product. Defaults to nil.
+    ///   - variant: Optional. The product variant. Defaults to nil.
+    ///   - position: Optional. The position of the product. Defaults to nil.
+    ///   - properties: Optional. The properties. Defaults to nil.
     public init(productName: String,
                 productId: String,
                 quantity: Int,
                 price: Double?,
+                brand: String? = nil,
+                category: String? = nil,
+                couponCode: String? = nil,
+                variant: String? = nil,
+                position: UInt? = nil,
                 properties: Properties?) {
         self.productName = productName
         self.productId = productId
         self.quantity = quantity
         self.price = price
+        self.brand = brand
+        self.category = category
+        self.couponCode = couponCode
+        self.variant = variant
+        self.position = position
         self.properties = properties
     }
 }
